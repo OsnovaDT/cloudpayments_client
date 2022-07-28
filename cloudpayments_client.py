790a8e20d13d7ba1079aa19fd4e3336d3cb21288
Evgeny Stepanov <maestrojane@gmail.com>
2022-07-28 16:43:59 +0300

    Add test method + refactoring

--- cloudpayments_client.py
+++ cloudpayments_client.py
@@ -2,7 +2,6 @@
 
 from asyncio import get_event_loop
 from base64 import b64encode
-from typing import Callable
 
 from aiohttp import TCPConnector
 
@@ -29,6 +28,13 @@ class CloudpaymentsAPIClient(AbstractInteractionClient):
 
         self.__send_request(charge_endpoint, 'post')
 
+    def test(self) -> None:
+        """Implement test method - check interaction with API"""
+
+        test_endpoint = self.endpoint_url('test/')
+
+        self.__send_request(test_endpoint, 'post')
+
     @property
     def __credentials(self) -> str:
         """Return decoded credentials (Public ID and API Secret)"""
@@ -37,14 +43,14 @@ class CloudpaymentsAPIClient(AbstractInteractionClient):
 
         return b64encode(creds.encode()).decode("ascii")
 
-    def __run_http_method(self, http_method: Callable, **kwargs) -> None:
-        """Run HTTP method in event loop"""
+    def __send_post(self, **kwargs) -> None:
+        """Run POST method in event loop"""
 
         event_loop = get_event_loop()
 
         try:
             event_loop.run_until_complete(
-                http_method(interaction_method='', **kwargs),
+                self.post(interaction_method='', **kwargs),
             )
         except InteractionResponseError as error:
             print(error)
@@ -53,37 +59,20 @@ class CloudpaymentsAPIClient(AbstractInteractionClient):
         self.CONNECTOR.close()
         event_loop.close()
 
-    def __send_request(
-            self, url: str, http_method_name: str, request_id=None) -> None:
+    def __send_request(self, url: str, request_id=None) -> None:
         """Send request to the URL with the HTTP method"""
 
-        if http_method := self.__get_http_method(name=http_method_name):
-            headers = {
-                'Authorization': f'Basic {self.__credentials}',
-            }
-
-            if request_id:
-                headers['X-Request-ID'] = request_id
-
-            self.__run_http_method(http_method, url=url, headers=headers)
-
-    def __get_http_method(self, name: str) -> Callable | None:
-        """Return HTTP method by name from AbstractInteractionClient"""
-
-        http_methods = {
-            # CRUD
-            'post': self.post,
-            'get': self.get,
-            'put': self.put,
-            'delete': self.delete,
-
-            # Others
-            'patch': self.patch,
+        headers = {
+            'Authorization': f'Basic {self.__credentials}',
         }
 
-        return http_methods.get(name)
+        if request_id:
+            headers['X-Request-ID'] = request_id
+
+        self.__send_post(url=url, headers=headers)
 
 
 if __name__ == "__main__":
     cloudpayments_client = CloudpaymentsAPIClient("public_id", "api_secret")
-    cloudpayments_client.charge()
+    # cloudpayments_client.charge()
+    cloudpayments_client.test()
